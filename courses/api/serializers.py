af489b3e30f380bb49e8165eae7bb954cb9a2265
davis <davisochieng4@gmail.com>
2024-09-21 03:40:13 +0300

    Added an api endpoint for course content

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,9 +1,64 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
-from courses.models import Subject
+from courses.models import Subject, Module, Course, Content
+
+User = get_user_model()
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['id', 'course', 'title', 'description']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'is_staff', 'is_superuser', 'is_active', 'last_login']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
+    owner = UserSerializer()
+    subject = SubjectSerializer()
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
+
+
+class ItemRelatedSerializer(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedSerializer(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+
+class ModuleWithContentSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
 
 from . import views
 
 app_name = 'courses'
 
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
+
 urlpatterns = [
     path('subjects/', views.SubjectListAPIView.as_view(), name='subject_list'),
     path('subject/<pk>', views.SubjectDetailAPIView.as_view(), name='subject_detail'),
+    path('courses/<pk>/enroll/', views.CourseEnrollAPIView.as_view(), name='course_enroll'),
+    path('', include(router.urls))
 ]
